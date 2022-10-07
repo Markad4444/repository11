0c47608386a9744d32ae310ea590d04037eb7735
vitthal <vitthalmarkad4444@gmail.com>
2022-10-07 19:30:08 +0530

    one more syso added

--- ClassAAAA.java
+++ ClassAAAA.java
@@ -3,8 +3,10 @@ package newprojectpkg;
 public class ClassAAAA {
 	public static void main(String[] args) {
 		
-	system.out.println("Hello word");
+	System.out.println("Hello word");
+	System.out.println("Hello word");
 	
 	}
 
 }
+ 
